dbb67bcd8d2c877592120273e00acf6a65d3daf1
jalvarenga-unah <jealvarengar@unah.edu.hn>
2023-07-06 13:31:09 -0600

    metodos con js

--- principios-js/js/operaciones.js
+++ principios-js/js/operaciones.js
@@ -25,4 +25,51 @@ const resta = () => {
   lblResultado.innerHTML = `El resultado es: ${resulatdo}`;
 };
 
+const multi = () => {
+  let num1 = Number(numero1.value);
+  let num2 = Number(numero2.value);
+
+  let resulatdo = num1 * num2;
+
+  lblResultado.innerHTML = `El resultado es: ${resulatdo}`;
+};
+
 btnResta.addEventListener("dblclick", resta);
+
+const operaciones = ({ tipo, num1, num2 }) => {
+  let resultado = 0;
+
+  // switch (tipo) {
+  //   case "suma":
+  //     resultado = num1 + num2;
+  //     break;
+  //   case "resta":
+  //     resultado = num1 - num2;
+  //     break;
+  //   case "multi":
+  //     resultado = num1 * num2;
+  //     break;
+  //   case "divi":
+  //     resultado = num1 / num2;
+  //     break;
+  // }
+
+  // if (tipo === "suma") {
+  //   resultado = num1 + num2;
+  // } else if (tipo === "resta") {
+  //   resultado = num1 - num2;
+  // } else if (tipo === "multi") {
+  //   resultado = num1 * num2;
+  // } else if (tipo === "divi") {
+  //   resultado = num1 / num2;
+  // }
+
+  const acciones = {
+    suma: () => num1 + num2,
+    resta: num1 - num2,
+    multi: num1 * num2,
+    divi: num1 / num2 ?? 1,
+  };
+
+  return acciones[tipo];
+};
